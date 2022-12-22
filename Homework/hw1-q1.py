a2da18e6d129646f8254a77e4bdd62c0196d1457
Guilherme Pereira <gcn.pereira@campus.fct.unl.pt>
2022-12-22 01:33:13 +0000

    Auto stash before merge of "main" and "origin/main"

--- Homework/hw1-q1.py
+++ Homework/hw1-q1.py
@@ -14,104 +14,10 @@ import torch.nn as nn
 
 import utils
 
-
 def configure_seed(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
     random.seed(seed)
     np.random.seed(seed)
-
-def relu(z):
-    return np.maximum(0, z)
-
-def softmax(z):
-    """
-    z - (number of classes x number of examples)
-    """
-    z -= np.max(z) # avoids overflow
-    Z = (np.exp(z)).sum(axis=0) # (1 x n_examples)
-    output_probabilities = np.exp(z) / Z # (n_classes x n_examples)
-    return output_probabilities
-
-
-def reluDerivative(z): 
-    z[z <= 0] = 0
-    z[z > 0] = 1
-    return z
-
-def forward(x, w, b):
-    """
-    Implements the linear pre-activation of a layer
-    W - weights matrix - (size of current layer x size of previous layer)
-    h - results from previous layer (or input data) - (number of examples x size of previous layer)
-    b - bias vector - (size of the current layer)
-    """
-    nLayers = len(w)
-    hiddens = []
-    x = x.reshape(-1, 1)
-
-    for i in range(nLayers):
-        h = x if i == 0 else hiddens[i -1]
-        print(h.shape)
-        print(w[i].shape)
-        print(b[i].shape)
-        z = w[i].dot(h) + b[i]
-        #import ipdb; ipdb.set_trace()
-        if(i < nLayers - 1): 
-            hiddens.append(relu(z)) # activate the layers except the output layers
-    print(z.shape)
-    print(x.shape)
-
-    return z, hiddens
-
-def backward(x, y, z, hiddens, W, B):
-    nLayers = len(W)
-    
-    probs = softmax(z)
-    gradZ = probs - y
-    #print(probs.shape)
-    #print(y.shape)
-    #print(gradZ.shape)
-    
-    gradW = []
-    gradB = []
-
-    for i in range(nLayers -1):
-        h = x if i == 0 else hiddens[i -1]
-        print(i)
-        gradW.append(np.dot(gradZ[:, None], h[:, None].T))
-        gradB.append(gradZ)
-
-        gradH = np.dot(W[i].T, gradZ)
-
-        gradZ = gradH * reluDerivative(gradZ)
-
-    gradW.reverse()
-    gradB.reverse()
-
-    return gradW, gradB
-
-def cross_entropy_loss(y_probabilities, y):
-    """
-    y_probabilities - probability vector from our prediction (n_examples)
-    y - gold label, one-hot vector (n_examples)
-    """
-    print(y)
-    print(y_probabilities)
-    #import ipdb; ipdb.set_trace()
-
-    one_hot_index = y[0]
-    y = np.zeros((10,1))
-    y[one_hot_index] = 1
-
-    #import ipdb; ipdb.set_trace()
-
-    loss = np.dot(-y.T, np.log(y_probabilities))
-    return loss
-
-def predict_label(z):
-    y_hat = np.zeros_like(z)
-    y_hat[np.argmax(z, axis=0)] = 1
-    return y_hat
 
 class LinearModel(object):
     def __init__(self, n_classes, n_features, **kwargs):
@@ -189,18 +95,15 @@ class MLP(object):
     # linear models with no changes to the training loop or evaluation code
     # in main().
     def __init__(self, n_classes, n_features, hidden_size):
-        # Initialize weight matrices with normal distribution N(mu, sigma^2)
         mu, sigma = 0.1, 0.1
 
-        W1 = np.random.normal(mu, sigma, hidden_size * n_features)
-        W1 = np.reshape(W1, (hidden_size, n_features)) # (hidden_size x n_features)
-
+        # Initialize weight matrices with normal distribution N(mu, sigma^2)
+        W1 = np.random.normal(mu, sigma, size = (hidden_size, n_features))
         W2 = np.random.normal(mu, sigma, n_classes * hidden_size)
-        W2 = np.reshape(W2, (n_classes, hidden_size)) # (n_classes x hidden_size)
         
         # Initialize bias to zeroes vector
-        b1 = np.zeros((hidden_size, 1)) # (hidden_size)
-        b2 = np.zeros((n_classes, 1)) # (n_classes)
+        b1 = np.zeros(hidden_size) # (hidden_size)
+        b2 = np.zeros(n_classes) # (n_classes)
 
         self.weights = [W1, W2]
         self.biases = [b1, b2]
@@ -213,65 +116,106 @@ class MLP(object):
         # no need to save the values of hidden nodes, whereas this is required
         # at training time.
 
-        predicted_labels = []
+        predictedLabels = []
 
         for x in X:
-            output, _ = forward(x, self.weights, self.biases)
-            y_hat = predict_label(output)
-            predicted_labels.append(y_hat)
+            output, _ = self.forward(x)
+            y_hat = self.predictLabel(output)
+            predictedLabels.append(y_hat)
 
-        predicted_labels = np.array(predicted_labels)
-
-        return predicted_labels
+        return predictedLabels
 
     def evaluate(self, X, y):
         """
         X (n_examples x n_features)
         y (n_examples): gold labels
         """
-        # Identical to LinearModel.evaluate()
-        predicted_labels = self.predict(X)
-        acc = np.mean(np.argmax(predicted_labels, axis = 0) == np.argmax(y, axis = 0))
+        predictedLabels = self.predict(X)
+        acc = np.mean(np.argmax(predictedLabels, axis = 1) == np.argmax(y, axis = 1))
 
         return acc
 
-    def update_weights(self, gradW, gradB, learning_rate):
+    def train_epoch(self, X, y, learning_rate=0.001):
+        for x_i, y_i in zip(X, y):
+            self.update_weights(x_i, y_i, learning_rate)
+    
+    def update_weights(self, x, y, eta):
         nLayers = len(self.weights)
         print(nLayers)
         print(len(gradB))
         print(len(gradW))
 
+        z, hiddens = self.forward(x)
+
+        gradWeights, gradBiases = self.backward(x, y, z, hiddens)
+
         for i in range(nLayers):
-            self.weights[i] -= learning_rate * gradW[i]
-            self.biases[i] -= learning_rate * gradB[i]
+            self.weights[i] -= eta * gradWeights[i]
+            self.biases[i] -= eta * gradBiases[i]
 
+    def relu(z):
+        return np.maximum(0, z)
 
-    def train_epoch(self, X, y, learning_rate=0.001):
-        totalLoss = 0
-
-        print("X shape")
-        print(X.shape)
-
-        y = y.reshape(-1, 1)
-        print("y shape")
-        print(y.shape)
-
-
-        for x, yy in zip(X, y):
-            x = x.reshape(-1, 1)
-            yy = yy.reshape(-1, 1)
-
-            output, hiddens = forward(x, self.weights, self.biases)
-            print("ys")
-            print(x.shape)
-            print(yy.shape) # SOMETIMES YY IS ()
-            loss = cross_entropy_loss(softmax(output), yy)
-            totalLoss += loss
-            gradWeights, gradBiases = backward(x, yy, output, hiddens, self.weights, self.biases)
-            self.update_weights(gradWeights, gradBiases, learning_rate)
+    def softmax(self, z):
+        probs = np.exp(z) / np.sum((np.exp(z)))
         
-        return loss
+        return probs
 
+    def forward(self, x):
+        nLayers = len(self.weights)
+        hiddenLayers = []
+
+        for i in range(nLayers):
+            h = x if i == 0 else hiddenLayers[i - 1]
+            z = np.dot(self.weights[i], h) + self.biases[i]
+
+            # If it isn't the last layer -> activation
+            if(i < nLayers - 1):
+                hiddenLayers.append(self.relu(z))
+        output = z
+
+        return output, hiddenLayers
+
+    def backward(self, x, y, output, hiddens):
+        nLayers = len(self.weights)
+        z = output
+
+        ## Cross-entropy loss function
+        probs = self.softmax(z)
+        gradZ = probs - y
+
+        gradWeights = []
+        gradBiases = []
+
+        ## for(i = nLayers - 1, i > -1, i--)
+        ## Basically a backwards "for" to access the hidden layers in the correct order
+        for i in range(nLayers -1, -1):
+            h = x if i == 0 else hiddens[i -1]
+
+            # Gradient of the current layer
+            gradWeights.append(np.dot((gradZ.shape[0], 1), np.matrix.getT(h)))
+            gradBiases.append(gradZ)
+
+            # Gradient of the previous layer
+            gradH = np.dot(np.matrix.getT(self.weights[i]), np.reshape(gradZ, (gradZ.shape[0], 1)))
+
+            gradZ = gradH * np.reshape(self.reluDerivative(h), (self.reluDerivative(h).shape[0], 1))
+
+        gradWeights.reverse()
+        gradBiases.reverse()
+
+        return gradWeights, gradBiases
+
+    def predictLabel(self, output):
+        y_hat = np.zeros_like(output)
+        y_hat[np.argmax(output)] = 1
+
+        return y_hat
+
+    def reluDerivative(z): 
+        z[z <= 0] = 0
+        z[z > 0] = 1
+        return z
 
 def plot(epochs, valid_accs, test_accs):
     plt.xlabel('Epoch')
